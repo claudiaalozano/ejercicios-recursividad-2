0aec4ed385742f8379e65774b4df7f2e057db9fb
claudiaalozano <91722847+claudiaalozano@users.noreply.github.com>
2022-02-27 19:48:38 +0000

    Ejercicio 2 completado

---
+++ ejercicio2.py
@@ -0,0 +1,23 @@
+entrada = input("Introduzca la cadena que desea analizar  comprobar si es un palíndromo: ")
+entrada.lstrip()
+palindromo(entrada)
+def palindromo (algo):
+  lista = list(entrada)
+def notilde(s)
+  remplazar = [("á" , "a"),
+           ("é" , "e"),
+           ("í" , "i"),
+           ("ú" , "u"),
+           ("ó" , "o")]
+  for entrada in remplazar :
+    s = s.raplace(entrada)
+
+revertir_cadena=[]
+i= len(entrada)
+while i > 0:
+  revertir_cadena += entrada[i-1]
+  i= i - 1
+if revertir_cadena is True:
+  print("La cadena de texto que usted ha introducido, es un palindromo")
+else:
+  print("La cadena de texto que usted ha introducido, no es un palindromo")
