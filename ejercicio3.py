548c275e0ece567370f699cb62154e34b6396748
claudiaalozano <91722847+claudiaalozano@users.noreply.github.com>
2022-02-27 21:54:28 +0000

    defino la función ordenar para eliminar los elementos de la lista y añadirlos en sus respectivas

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,1 +1,8 @@
-from random import randint
+bandera_dijkstra = []
+fichas = ["V" , "R" , "A" , "A" , "R" , "V" , "R" , "A" , "R" , "V" , "V"]
+A = []
+V = []
+R = []
+def ordenar (bandera_dijkstra):
+  if len(bandera_dijkstra) > 0:
+    
