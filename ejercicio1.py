47fd2af9eb0bf7c12f5372aa7cfa68a46a22b611
claudiaalozano <91722847+claudiaalozano@users.noreply.github.com>
2022-02-27 20:17:39 +0000

    ejercicio 1 completado

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,4 +1,12 @@
-buscar(["gato" , "ángel" , "fiesta" , "senderismo" , "pan" , "bebida"], palabra, 0 )
+
+lista = ["gato" , "ángel" , "fiesta" , "senderismo" , "pan" , "bebida"]
+indice = 0
 palabra = input("Seleccione la palabra que desea buscar en la tabla: ")
 
-def buscar (tabla, palabra, indice):
+def buscar (lista, palabra, indice):
+  if palabra == str(lista[indice]):
+    print("Se encuentra en la tabla")
+  else:
+    if indice < (len(tabla)-1):
+      indice = indice +1
+      buscar(lista, palabra, indice)
