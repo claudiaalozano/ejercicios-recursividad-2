87b6acddecd13ceaf897f69349329f4bd8548b56
claudiaalozano <91722847+claudiaalozano@users.noreply.github.com>
2022-02-27 20:07:46 +0000

    defino la función buscar, para encontrar la palabra

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,3 +1,4 @@
 buscar(["gato" , "ángel" , "fiesta" , "senderismo" , "pan" , "bebida"], palabra, 0 )
 palabra = input("Seleccione la palabra que desea buscar en la tabla: ")
 
+def buscar (tabla, palabra, indice):
