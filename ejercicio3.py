a618a447a5b666406e323761c63f49b9fa1d92ba
claudiaalozano <91722847+claudiaalozano@users.noreply.github.com>
2022-02-27 22:06:51 +0000

    ejercicio 3 terminado

--- ejercicio3.py
+++ ejercicio3.py
@@ -5,4 +5,18 @@ V = []
 R = []
 def ordenar (bandera_dijkstra):
   if len(bandera_dijkstra) > 0:
-    
+    color= bandera.pop(0)
+    if color == "A":
+      A.append(color)
+      ordenar(bandera_dijkstra)
+    if color == "V":
+      V.append(color)
+      ordenar(bandera_dijkstra)
+    if color == "R":
+      R.append(color)
+      ordenar(bandera_dijkstra)
+  else:
+    ordenada = A + V + R
+    print ("La bandera ya esta ordenada, " , ordenada)
+ordenar(bandera_dijkstra)
+ 
